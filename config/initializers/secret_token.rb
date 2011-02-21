d2bf91d9d474baff5156700b256b6a0f79254ce3
Reinaldo Junior <juniorz@gmail.com>
2011-02-21 20:36:33 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mercatus::Application.config.secret_token = '243211f51d9f6b692324d3d826dda69ff73b99dc20ae7158750f694e7cd0edda2d5bae3acc9afbc70fbe4fb61290d278ba2abf0422652fad17708ca3a498720d'
